6cae9568edd42243e11966e44f0b334dfb7d4e00
Randy Secrist <randy.secrist@gmail.com>
2009-10-31 08:51:30 -0600

    Updated comments to better show what is going on.

--- Resolver.py
+++ Resolver.py
@@ -23,7 +23,7 @@ def error_msg(msg):
     
 class EventHandlers():
     def new_service_type(self, interface, protocol, stype, domain, flags):
-        print "--- Hi! ---"
+        print "--- New Service Type Called! ---"
         
         
 def main():
@@ -33,6 +33,9 @@ def main():
     bus = dbus.SystemBus()
     server = dbus.Interface(bus.get_object(avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER), avahi.DBUS_INTERFACE_SERVER)
     try:
+        # how do we know to limit this range?
+        # which NIC do we listen on?
+        # do we even need to do this?
         for i in range(1, 8):
             x = server.GetNetworkInterfaceNameByIndex(i)
             print x
@@ -46,17 +49,15 @@ def main():
         protocol = avahi.PROTO_UNSPEC
         b = dbus.Interface(bus.get_object(avahi.DBUS_NAME, server.ServiceTypeBrowserNew(interface, protocol, domain, dbus.UInt32(0))), avahi.DBUS_INTERFACE_SERVICE_TYPE_BROWSER)
 
-        
-
-        
         # register for dbus events
         #b.connect_to_signal('ItemNew', self.new_service_type)
         # line 168 of avahi-discover
+        # basically - sets up a function pointer to ItemNew signal
         b.connect_to_signal('ItemNew', handlers.new_service_type)
         
         service_type_browsers[(interface, protocol, domain)] = b
+        # do we have to do anything else to start up the listener?
         print service_type_browsers
-
 
     except DBusException, e:
         print "DBusException - %s" % e
@@ -65,5 +66,5 @@ def main():
 if __name__ == "__main__":
     main()
     import time
-    time.sleep(10)
+    time.sleep(15)
     print "All Done!"
